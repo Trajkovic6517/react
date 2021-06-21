78df02695bb9088249d38a8adef69efaaafccff4
Teodora Trajkovic <tea.trajkovic@yahoo.com>
2021-06-21 14:49:35 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Forma from './components/Forma';
+import Spisak from './components/Spisak';
+
+function App() {
+  const [studenti, setStudenti] = useState([]);
+  const kreiraj = (student) => {
+    setStudenti(prev => [...prev, student]);
+    alert(' uspesno kreiran student sa indeksom ' + student.indeks)
+  }
+
+  return (
+    <BrowserRouter>
+
+      <Switch>
+
+        <Route path='/spisak'>
+          <Spisak studenti={studenti} />
+        </Route>
+        <Route path='/'>
+          <Forma kreiraj={kreiraj} />
+        </Route>
+      </Switch>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
